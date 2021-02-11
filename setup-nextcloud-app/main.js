1cdd0c878139ad5c1b702946aae206c8698a0cea
Benjamin Brahmer <info@b-brahmer.de>
2021-02-11 08:41:40 +0100

    Add option to force enable an app

--- setup-nextcloud-app/main.js
+++ setup-nextcloud-app/main.js
@@ -5,13 +5,19 @@ async function main() {
     try {
         const app       = core.getInput("app", {required: true})
         const check     = core.getInput("check-code", {required: true})
+        const force     = core.getInput("force", {required: false})
         const serverDir = '../server'
 
         await exec.exec("mkdir", ["-p",`${serverDir}/apps/${app}`])
         await exec.exec("cp", ["-R", "./", `${serverDir}/apps/${app}`])
 
         process.chdir(serverDir)
-        await exec.exec("./occ", ["app:enable", app])
+
+        if (force) {
+            await exec.exec("./occ", ["-f", "app:enable", app])
+        }else {
+            await exec.exec("./occ", ["app:enable", app])
+        }
 
         if (check) {
             await exec.exec("./occ", ["app:check-code", app])
